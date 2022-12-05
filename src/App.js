01050e529080bcdeedb181ebe6f9243289acab7f
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-12-05 16:29:12 -0700

    3. Create Client

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+import { SignClient } from "@walletconnect/sign-client";
 import "./App.css";
 
 function App() {
+  const [signClient, setSignClient] = useState();
+
+  async function createClient() {
+    try {
+      const client = await SignClient.init({
+        projectId: process.env.REACT_APP_PROJECT_ID,
+      });
+      setSignClient(client);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    if (!signClient) {
+      createClient();
+    }
+  }, [signClient]);
+
   return (
     <div className="App">
       <h1>Youtube Tutorial</h1>
